alezost/imenus.el

commit c0ef6cd7b96f068a36df61134d249785daba2b4c

Do not make *Rescan* item an input after rescanning
Before this fix, after selecting *Rescan* item, the index is rescanned
and a new imenus prompt appears where *Rescan* item becomes an initial
input, which should not happen.

imenus.el
@@ -286,7 +286,7 @@ Interactively, use the current buffer."
     (cond ((or (eq imenus-exit-status 'rescan)
                (equal input imenu--rescan-item))
            (imenu--cleanup index)
-           (imenus-buffers buffers 'rescan prompt input))
+           (imenus-buffers buffers 'rescan prompt initial-input))
           (imenus-exit-status
            (let ((fun (cdr (assq imenus-exit-status imenus-actions))))
              (and fun (funcall fun buffers input))))

commit 33820c64d4c440620c3c204cfbaca885e426aa43

Display initial input after rescanning with M-r
This fixes a regression introduced in commit
c0ef6cd7b96f068a36df61134d249785daba2b4c.

I forgot that there are 2 ways to rescan an imenus index:

1. By pressing "M-r": in this case a user input should reappear in the
   minibuffer.

2. By selecting *Rescan* item: in this case an input (which is
   "*Rescan*" itself) shouldn't appear in the minibuffer.

Since the very beginning only the first variant was handled properly.
After commit c0ef6cd7, the second variant began to work as expected but
not the first one as the user input was removed.  Now both should work.

imenus.el
@@ -275,8 +275,10 @@ current input.
 Interactively, use the current buffer."
   (let* ((index (imenus-buffers-index buffers rescan))
          (input (imenus-completing-read index prompt initial-input)))
-    (cond ((or (eq imenus-exit-status 'rescan)
-               (equal input imenu--rescan-item))
+    (cond ((eq imenus-exit-status 'rescan)
+           (imenu--cleanup index)
+           (imenus-buffers buffers 'rescan prompt input))
+          ((equal input imenu--rescan-item)
            (imenu--cleanup index)
            (imenus-buffers buffers 'rescan prompt initial-input))
           (imenus-exit-status